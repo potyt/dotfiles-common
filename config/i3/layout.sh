38c83774e278b0559b2c432ff3185b486a7f0ded
pot <p@pot.yt>
2014-09-09 18:26:40 +0100

    Move workspaces to specified outputs.

--- config/i3/layout.sh
+++ config/i3/layout.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
 
-. ~/.config/i3/auto-scratchpad.sh
+i=$1
+o=$2
+shift 2
 
-i=1
 for n in "$@"; do
     layout=~/.config/i3/layout-$n.json
     auto=~/.config/i3/auto-$n.sh
     if [[ -r $layout && -r $auto ]]; then
         w="$i:$n"
-        i3-msg "workspace $i, rename workspace to $w"
+        i3-msg "workspace $w, move workspace to output $o"
         i3-msg "append_layout $layout"
         . $auto
-
         let i=i+1
     fi
 done
